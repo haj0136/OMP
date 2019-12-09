57d4cb178f9d865a27fd24711c278ffdcfd09478
maras49 <hajdk@seznam.cz>
2019-12-09 22:20:38 +0100

    load and save file

--- OpticalMappingParser.Gui/Form1.Designer.cs
+++ OpticalMappingParser.Gui/Form1.Designer.cs
@@ -35,7 +35,20 @@
             this.saveFileToCSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownMaxSeqNoMarks = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownConsecutiveMarks = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownMaxSeqBetweenMarks = new System.Windows.Forms.NumericUpDown();
+            this.generateButton = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMaxSeqNoMarks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownConsecutiveMarks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMaxSeqBetweenMarks)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -69,6 +82,7 @@
             this.saveFileToCSVToolStripMenuItem.Name = "saveFileToCSVToolStripMenuItem";
             this.saveFileToCSVToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveFileToCSVToolStripMenuItem.Text = "Save File to CSV";
+            this.saveFileToCSVToolStripMenuItem.Click += new System.EventHandler(this.saveFileToCSVToolStripMenuItem_Click);
             // 
             // openFileDialog1
             // 
@@ -76,27 +90,130 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.generateButton);
+            this.groupBox1.Controls.Add(this.numericUpDownMaxSeqBetweenMarks);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericUpDownConsecutiveMarks);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.numericUpDownMaxSeqNoMarks);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(13, 28);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(223, 410);
+            this.groupBox1.Size = new System.Drawing.Size(241, 410);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Parametrs";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Maximal sequence length without marks:";
+            // 
+            // numericUpDownMaxSeqNoMarks
+            // 
+            this.numericUpDownMaxSeqNoMarks.Increment = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownMaxSeqNoMarks.Location = new System.Drawing.Point(10, 45);
+            this.numericUpDownMaxSeqNoMarks.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDownMaxSeqNoMarks.Name = "numericUpDownMaxSeqNoMarks";
+            this.numericUpDownMaxSeqNoMarks.Size = new System.Drawing.Size(192, 20);
+            this.numericUpDownMaxSeqNoMarks.TabIndex = 1;
+            this.numericUpDownMaxSeqNoMarks.ThousandsSeparator = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number of consecutive makrs:";
+            // 
+            // numericUpDownConsecutiveMarks
+            // 
+            this.numericUpDownConsecutiveMarks.Location = new System.Drawing.Point(10, 97);
+            this.numericUpDownConsecutiveMarks.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownConsecutiveMarks.Name = "numericUpDownConsecutiveMarks";
+            this.numericUpDownConsecutiveMarks.Size = new System.Drawing.Size(192, 20);
+            this.numericUpDownConsecutiveMarks.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(260, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(528, 410);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Maximal sequence length between marks:";
+            // 
+            // numericUpDownMaxSeqBetweenMarks
+            // 
+            this.numericUpDownMaxSeqBetweenMarks.Location = new System.Drawing.Point(10, 151);
+            this.numericUpDownMaxSeqBetweenMarks.Name = "numericUpDownMaxSeqBetweenMarks";
+            this.numericUpDownMaxSeqBetweenMarks.Size = new System.Drawing.Size(195, 20);
+            this.numericUpDownMaxSeqBetweenMarks.TabIndex = 5;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.generateButton.Enabled = false;
+            this.generateButton.Location = new System.Drawing.Point(6, 381);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(229, 23);
+            this.generateButton.TabIndex = 6;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Optical Mapping Parser";
+            this.Text = "Difficult area indentifier";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMaxSeqNoMarks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownConsecutiveMarks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMaxSeqBetweenMarks)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,6 +227,14 @@
         private System.Windows.Forms.ToolStripMenuItem saveFileToCSVToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDownMaxSeqNoMarks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownConsecutiveMarks;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownMaxSeqBetweenMarks;
+        private System.Windows.Forms.Button generateButton;
     }
 }
 
--- OpticalMappingParser.Gui/Form1.cs
+++ OpticalMappingParser.Gui/Form1.cs
@@ -15,6 +15,8 @@ namespace OpticalMappingParser.Gui
         {
             InitializeComponent();
             openFileDialog1.Filter = "cmap files (*.cmap)|*.cmap";
+            numericUpDownMaxSeqNoMarks.Maximum = decimal.MaxValue;
+            numericUpDownMaxSeqBetweenMarks.Maximum = decimal.MaxValue;
         }
 
         private void LoadFileToolStripMenuItem_Click(object sender, EventArgs e)
@@ -25,12 +27,39 @@ namespace OpticalMappingParser.Gui
                 {
                     var fileName = openFileDialog1.FileName;
                     _parser = new DifficultAreaIdentifier(fileName);
+                    generateButton.Enabled = true;
                 }
                 catch (Exception exception)
                 {
                     MessageBox.Show($"File load error. \n\n Error message: {exception.Message}");
                 }
             }
+        }
+
+        private void saveFileToCSVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "csv files (*.csv)|*.csv";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _parser.SaveToCsv(saveFileDialog.FileName);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var maxLenght = numericUpDownMaxSeqNoMarks.Value;
+            var marksCount = numericUpDownConsecutiveMarks.Value;
+            var minLenght = numericUpDownMaxSeqBetweenMarks.Value;
+            var resultList = _parser.Process((int)maxLenght, (int)marksCount, (int)minLenght);
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "Chromosome";
+            dataGridView1.Columns[1].Name = "Start position";
+            dataGridView1.Columns[2].Name = "End position";
+            dataGridView1.Columns[3].Name = "Type";
+
         }
     }
 }
